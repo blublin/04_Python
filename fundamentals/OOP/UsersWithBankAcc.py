77e82f104cfb83376d3dc68c1fbc7c78d04a1733
blublin <lublin.ben@gmail.com>
2022-04-04 13:29:27 -0700

    Added modular parent/child py files

--- fundamentals/OOP/UsersWithBankAcc.py
+++ fundamentals/OOP/UsersWithBankAcc.py
@@ -16,8 +16,9 @@ class BankAccount:
             self.balance -= 5
         return self
     def display_account_info(self):
-        print(f"Balance: ${self.balance:,}")
-        return self
+        # print(f"Balance: ${self.balance:,}")
+        # return self
+        return self.balance
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance * self.intRate
@@ -31,24 +32,34 @@ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = BankAccount(3,100)
+        self.accounts = {} # dictionary holding account names (keys) and amounts (values)
 
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
+    def make_account(self, accName, intRate, balance = 0):
+        self.accounts[accName] = BankAccount(intRate, balance)
+    def make_deposit(self, accName, amount):
+        self.accounts[accName].deposit(amount)
         return self
-    def make_withdrawal(self, amount):
-        self.account.withdraw(amount)
+    def make_withdrawal(self, accName, amount):
+        self.accounts[accName].withdraw(amount)
         return self
-    def display_user_balance(self):
-        self.account.display_account_info()
+    def list_accounts(self):
+        for acc in self.accounts:
+            print(f"Account: {acc}")
+    def display_user_balance(self, accName=''):
+        if accName:
+            print(f"User: {self.name}, {accName} Balance: ${self.accounts[accName].display_account_info():,}")
+        else:
+            for accName in self.accounts:
+                print(f"User: {self.name}, {accName} Balance: ${self.accounts[accName].display_account_info():,}") 
+        # self.accounts[accName].display_account_info()
         return self
-    def transfer_money(self, other_user, amount):
-        if self.account.balance >= amount:
-            self.account.withdraw(amount)
-            other_user.account.deposit(amount)
-            print(f"Successfully transfered ${amount} from {self.name} to {other_user.name}.")
-            self.account.display_account_info()
-            other_user.account.display_account_info()
+    def transfer_money(self, accName, other_user, other_user_acc, amount):
+        if self.accounts[accName].balance >= amount:
+            self.accounts[accName].withdraw(amount)
+            other_user.accounts[other_user_acc].deposit(amount)
+            print(f"Successfully transfered ${amount} from {self.name}'s {accName} account to {other_user.name}'s {other_user_acc} account.")
+            self.accounts[accName].display_account_info()
+            other_user.accounts[other_user_acc].display_account_info()
         else:
             print("Insufficient funds.")
         return self
@@ -57,10 +68,17 @@ ben = User("Ben Lublin", "lublin.ben@gmail.com")
 scsa = User("Stone Cold Steve Austin", "questions@steveaustinshow.com")
 hank = User("Hank Hill", "hill@stricklandpropane.com")
 
-ben.make_deposit(500).make_deposit(378).make_deposit(823).make_withdrawal(400).display_user_balance()
+ben.make_account("Savings", 3, 50829)
+scsa.make_account("Checking", 5, 2309233)
+hank.make_account("Propane", 2, 2000)
 
-scsa.make_deposit(39474762).make_deposit(47595733).make_withdrawal(23436742).make_withdrawal(12422382).display_user_balance()
+ben.make_deposit("Savings", 500).make_deposit("Savings", 378).make_deposit("Savings", 823).make_withdrawal("Savings", 400).display_user_balance()
 
-hank.make_deposit(500).make_withdrawal(50).make_withdrawal(50).make_withdrawal(125).display_user_balance()
+scsa.make_deposit("Checking", 39474762).make_deposit("Checking", 47595733).make_withdrawal("Checking", 23436742).make_withdrawal("Checking", 12422382).display_user_balance()
 
-ben.transfer_money(hank, 250)
+hank.make_deposit("Propane", 500).make_withdrawal("Propane", 50).make_withdrawal("Propane", 50).make_withdrawal("Propane", 125).display_user_balance()
+
+ben.transfer_money("Savings", hank, "Propane", 250)
+ben.transfer_money("Savings", hank, "Propane", 250)
+ben.transfer_money("Savings", hank, "Propane", 250)
+ben.transfer_money("Savings", hank, "Propane", 250)
