1f4206deefe55487d31e638bd9928e00c941aa48
blublin <lublin.ben@gmail.com>
2022-04-06 11:36:10 -0700

    Closer to checkerboard

--- Flask/fundamentals/Checkerboard/server.py
+++ Flask/fundamentals/Checkerboard/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/<int:x>/<int:y>')
 @app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
 def index(x=8, y=8, color1="black", color2="red"):
-    return render_template("index.html", x, y, color1, color2)
+    return render_template("index.html", x=x, y=y, color1=color1, color2=color2)
 
 @app.errorhandler(404)
 def fourZeroFour(err):
